c0818349a62d40a8e88e5accf934da3c8649c53e
ZwolinKox <zwolinn@wp.pl>
2020-02-29 22:26:04 +0100

    Add API call to login page

--- view/login.js
+++ view/login.js
@@ -6,6 +6,51 @@ $(document).ready(function(){
     $("#logo-text").fadeIn(1000);
     $("#login-form").fadeIn(1000);
     $("#register-form").fadeIn(1000);
-
-
   })
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#login-button').addEventListener('click', () => {
+
+        let data = {
+            login : document.querySelector('#loginL').value,
+            password : document.querySelector('#passwordL').value,
+        }
+
+        fetch('loginUrl', {
+            method : "post",
+            body : JSON.stringify(data)
+        }).then(res => res.json())
+        .then(res => {
+            if(res = "success")
+                 console.log('Login success')
+            else
+                document.querySelector("#login-error").innerHTML = "Login error!";
+
+        }).catch(error => {
+            document.querySelector("#login-error").innerHTML = "API call error! Status: " + error;
+        })
+    })
+
+    document.querySelector('#register-button').addEventListener('click', () => {
+
+        let data = {
+            login : document.querySelector('#login').value,
+            password : document.querySelector('#password').value,
+        }
+
+        fetch('registerUrl', {
+            method : "post",
+            body : JSON.stringify(data)
+        }).then(res => res.json())
+        .then(res => {
+            if(res = "success")
+                 console.log('Register success')
+            else
+                document.querySelector("#register-error").innerHTML = "Register error!";
+
+        }).catch(error => {
+            document.querySelector("#register-error").innerHTML = "API call error! Status: " + error;
+        })
+    })
+})
